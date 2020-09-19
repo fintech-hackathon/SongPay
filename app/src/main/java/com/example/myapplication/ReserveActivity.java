bbf392ac66c93a04ccc48231ce12e344154b76da
이원호 <dldnjsgh71@naver.com>
2020-09-19 22:40:48 +0900

    reseve

--- app/src/main/java/com/example/myapplication/ReserveActivity.java
+++ app/src/main/java/com/example/myapplication/ReserveActivity.java
@@ -1,6 +1,7 @@
 package com.example.myapplication;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.InputType;
 import android.util.Log;
@@ -15,6 +16,9 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class ReserveActivity extends AppCompatActivity {
 
 //    노래방이름 , 주소, 가격정보(DB에서?)
@@ -22,6 +26,7 @@ public class ReserveActivity extends AppCompatActivity {
     Button reserveButton;
     AutoCompleteTextView roomSelectDropdownMenu, songSelectDropdownMenu;
     TimePicker timePicker;
+    String room,songbymoney,owner,strTime;
 
     PopupMenu roomSelectMenu,songSelectMenu;
 
@@ -29,10 +34,10 @@ public class ReserveActivity extends AppCompatActivity {
         Intent intent = getIntent();
         String title = intent.getStringExtra("title");
         String address = intent.getStringExtra("address");
-        String songbymoney=intent.getStringExtra("songbymoney");
-        String roomnum = intent.getStringExtra("roomnum");
-        Log.i("msg", songbymoney);
-        Log.i("msg", roomnum);
+        room = intent.getStringExtra("room");
+        songbymoney = intent.getStringExtra("songbymoney");
+        owner = intent.getStringExtra("owner");
+
 
         titleTextView = findViewById(R.id.titleTextView);
         addressTextView = findViewById(R.id.addressTextView);
@@ -50,6 +55,7 @@ public class ReserveActivity extends AppCompatActivity {
 
         titleTextView.setText(title);
         addressTextView.setText(address);
+        priceInfoTextView.setText(songbymoney+"곡 : 1000원");
 
     }
 
@@ -63,22 +69,31 @@ public class ReserveActivity extends AppCompatActivity {
         timePicker.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
             @Override
             public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
-                String strTime = hourOfDay + " : " + minute;
+
+
+                Date time = new Date();
+                SimpleDateFormat sdp = new SimpleDateFormat("yyyy/MM/dd");
+                String dd = sdp.format(time);
+
+                strTime = dd+"/"+hourOfDay+"/"+minute;
+
                 Toast.makeText(getApplicationContext(), strTime, Toast.LENGTH_SHORT).show();
             }
         });
 
 
-        String[] RoomsArray = new String[]{"Item 1", "Item 2", "Item 3", "Item 4"};
+
 
         roomSelectDropdownMenu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 roomSelectMenu = new PopupMenu(getApplicationContext(), view);
 
-                roomSelectMenu.getMenu().add("1번방");
-                roomSelectMenu.getMenu().add("2번방");
-                roomSelectMenu.getMenu().add("3번방");
+                int roomnum = Integer.parseInt(room);
+
+                for(int i = 0;i < roomnum ; i++){
+                    roomSelectMenu.getMenu().add((i+1)+"번방");
+                }
                 roomSelectMenu.show();
 
                 roomSelectMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
@@ -96,10 +111,10 @@ public class ReserveActivity extends AppCompatActivity {
             public void onClick(View view) {
                 songSelectMenu = new PopupMenu(getApplicationContext(), view);
 
-                songSelectMenu.getMenu().add("3곡");
-                songSelectMenu.getMenu().add("5곡");
-                songSelectMenu.getMenu().add("10곡");
-                songSelectMenu.getMenu().add("30곡");
+                //x 곡
+                for(int i = 1;i <= 4; i++){
+                    songSelectMenu.getMenu().add((Integer.parseInt(songbymoney)*i)+"곡");
+                }
                 songSelectMenu.show();
 
                 songSelectMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
@@ -115,6 +130,24 @@ public class ReserveActivity extends AppCompatActivity {
         reserveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                SharedPreferences sharedPreferences= getSharedPreferences("User", MODE_PRIVATE);
+
+                String ID = sharedPreferences.getString("Id","default Name");  // 불러올려는 key, default Value
+                final String TYPE = "1";
+
+                Log.i("msg","ID : " + ID);
+                Log.i("msg","owner : "+owner);
+                Log.i("msg","songSelectDropdownMenu : "+songSelectDropdownMenu.getText());
+                Log.i("msg","roomSelectDropdownMenu : "+roomSelectDropdownMenu.getText());
+                Log.i("msg","room : "+room);
+                Log.i("msg","songbymoney : "+songbymoney);
+                Log.i("msg","TYPE : "+TYPE);
+                Log.i("msg","Date : "+strTime);
+
+
+
+
                 Toast.makeText(getApplicationContext(), "예약이 완료되었습니다!", Toast.LENGTH_SHORT).show();
                 finish();
                 // dialog로 표시
